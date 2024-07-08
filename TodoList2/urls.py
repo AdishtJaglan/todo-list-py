3bc8fd1be9492a766317e35e2e961a1622b3f633
AdishtJaglan <adisht7524@gmail.com>
2024-07-08 12:16:38 +0530

    update nav styles

--- TodoList2/urls.py
+++ TodoList2/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("todo/", include("todos.urls")),
+    path("todos/", include("todos.urls")),
 ]
 
 
